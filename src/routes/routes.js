4b9f4ca81962b93a6506e9b581600a3872a34d40
KevinSalinas <gabiand1.gs@gmail.com>
2024-09-24 18:58:44 -0300

    Se modularizo las el llamado a las rutas

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,16 +1,13 @@
 const express = require('express');
 const router = express.Router();
-const usuarioController = require('../controllers/usuario.controller')
-const verificarToken = require('../middleware/auth');
-
-router.post('/register', usuarioController.registroUsuario);
-router.post('/login', usuarioController.loginUsuario);
-router.get('/listar-usuario',  verificarToken, usuarioController.consultarUsuario);
 
 
-router.post("/requestResetPassword", usuarioController.resetPasswordRequestController1);
-router.post("/resetPassword", usuarioController.resetPasswordController1);
+//Subrutas
+const usuarioRuta = require('./usuario.route');
+//const  cursoRuta= require('curso')
 
-router.get('/verificarCuenta', usuarioController.verificarCuenta);
+
+router.use('/usuario', usuarioRuta);
+
 
 module.exports = router;
---
+++ src/routes/usuario.route.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const router = express.Router();
+const usuarioController = require('../controllers/usuario.controller')
+const verificarToken = require('../middleware/auth');
+
+router.post('/register', usuarioController.registroUsuario);
+router.post('/login', usuarioController.loginUsuario);
+router.get('/listar-usuario',  verificarToken, usuarioController.consultarUsuario);
+
+
+router.post("/requestResetPassword", usuarioController.resetPasswordRequestController1);
+router.post("/resetPassword", usuarioController.resetPasswordController1);
+
+router.get('/verificarCuenta', usuarioController.verificarCuenta);
+
+module.exports = router;
