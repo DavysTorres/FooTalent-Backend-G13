51cf1ffcba99fbfc8e57c4f8450222c5d7b58ce5
KevinSalinas <gabiand1.gs@gmail.com>
2024-10-02 19:59:46 -0300

    Documenta las funcionalidades de cada ruta

--- src/routes/curso.route.js
+++ src/routes/curso.route.js
@@ -3,12 +3,17 @@ const router = express.Router();
 
 const cursoController = require('../controllers/curso.controller')
 
-
+//Crear un curso
 router.post('/', cursoController.crearCurso);
+//Muestra todos los cursos
 router.get('/', cursoController.mostrarCurso);
+//Muestra un curso según el ID del curso
 router.get('/:id', cursoController.mostrarCursoPorId);
+//Edita la informacion de un curso
 router.put('/:id', cursoController.editarCurso);
+//Elimina un curso de una forma logica
 router.delete('/:id', cursoController.eliminarCurso);
+//Muestra un curso según el id de un usuario, ya sea profesor o alumno
 router.get('/cursos/:id', cursoController.mostrarCursosPorUsuario);
 
 module.exports = router;
--- src/routes/suscripcion.route.js
+++ src/routes/suscripcion.route.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 const suscripcionController = require('../controllers/suscripcion.controller')
 
-
+//genera una suscripcion/inscripcion de un estudiante a un curso
 router.post('/', suscripcionController.generarSuscripcion);
 
 module.exports = router;
