e59f61f9837cb8b237ed9d84108550c131ea8a3d
KevinSalinas <gabiand1.gs@gmail.com>
2024-10-11 22:14:46 -0300

    arreglo en endpoint verifyAccount

---
+++ src/routes/usuario.route.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const router = express.Router();
+const usuarioController = require('../controllers/usuario.controller')
+const verificarToken = require('../middleware/auth');
+const { cargarImagen } = require('../services/cargarArchivo.service');
+
+//Muestra todos los usuarios registrados
+router.get('/listarUsuarios', usuarioController.mostrarUsuarios);
+//Registra un nuevo usuario
+router.post('/register', usuarioController.registroUsuario);
+//Verifica los datos para poder iniciar sesion
+router.post('/login', usuarioController.loginUsuario);
+//Devuelve un usuario por el token recibido despues de loguearse (funcion para devolver los datos al modificar perfil)
+router.get('/listar-usuario', verificarToken, usuarioController.consultarUsuario);
+//Elimina un usuario de forma logica
+router.delete('/eliminar/:id', usuarioController.eliminarUsuario);
+//Edita un usuario
+router.put('/editar/:id', cargarImagen.single('avatar'), usuarioController.editarUsuario)
+//Peticion para comenzar un reestablecimiento de contraseña
+router.post("/requestResetPassword", usuarioController.resetPasswordRequestController1);
+//Reestablecimiento de contraseña 
+router.post("/resetPassword", usuarioController.resetPasswordController1);
+// Verifica la cuenta mediante un envio de correo electronico al crear una nueva cuenta
+router.get('/verifyAccount', usuarioController.verificarCuenta);
+
+module.exports = router;
