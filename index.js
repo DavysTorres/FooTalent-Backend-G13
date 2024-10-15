e6ec09a70934068c541f23247dc8931df324d807
DavisTorres <davis.940@gmail.com>
2024-10-14 23:48:19 -0500

    cambios en el index

--- index.js
+++ index.js
@@ -2,16 +2,37 @@ const express = require('express');
 const conectarDB = require('./src/config/configDB');
 const cors = require('cors');
 const app = express();
+const helmet = require('helmet');
 
 conectarDB();
 
+app.use(helmet({
+    contentSecurityPolicy: {
+      directives: {
+        defaultSrc: ["'self'"],
+        scriptSrc: ["'self'", "https://vercel.live", "https://your-frontend-domain.vercel.app"],
+        objectSrc: ["'none'"],
+      },
+    },
+  }));
+
+
+const origenesPermitidos = ['http://localhost:4200', 'https://foo-talent-backend-g13.vercel.app/'];
+
 //Middlewares
 app.use(cors({
-    origin: 'http://localhost:4200'
+    origin: function (origin, callback) {
+        if (!origin) return callback(null, true);
+        if (origenesPermitidos.indexOf(origin) === -1) {
+            const msg = 'La política CORS para este sitio no permite el acceso desde el Origen especificado.';
+            return callback(new Error(msg), false);
+        }
+        return callback(null, true);
+    },
+    credentials: true
 }));
+
 app.use(express.json());
-
-
 
 //Routes
 app.use('/', require('./src/routes/routes'));
